cb6033bb31d81a5048f2ee0f2680960193d04ae1
Henok <henokmicrometre@gmail.com>
2024-05-11 18:19:45 +0100

    connected mysql

---
+++ data-access/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+
+
+var db *sql.DB
+
+func main() {
+    // Capture connection properties.
+    cfg := mysql.Config{
+        User:   ("alpruser"),
+        Passwd: ("alpruser"),
+        Net:    "tcp",
+        Addr:   "127.0.0.1:3306",
+        DBName: "recordings",
+        AllowNativePasswords: true,
+
+    }
+    // Get a database handle.
+    var err error
+    db, err = sql.Open("mysql", cfg.FormatDSN())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    pingErr := db.Ping()
+    if pingErr != nil {
+        log.Fatal(pingErr)
+    }
+    fmt.Println("Connected!")
+}
