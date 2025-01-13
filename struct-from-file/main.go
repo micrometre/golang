c657239c375845c58bf742a119562f8eb95548c1
Henok Wehibe <henokwehibe@gmail.com>
2025-01-13 21:28:56 +0000

    sorting

--- struct-from-file/main.go
+++ struct-from-file/main.go
@@ -8,18 +8,21 @@ import (
 )
 
 type Person struct {
-	Hobbies []string
+	Results []string
 }
 
+type Alprd struct {
+	Hobbies []string
+    Uuid   string `json:"uuid"`
+}
 func main() {
 	filePath := "db.json"
 	jsonData, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	jsonStr := "{\"name\": \"Prashant\", \"age\": 27, \"hobbies\":[\"sports\",\"music\"]}"
-	data := Person{}
-	err = json.Unmarshal([]byte(jsonStr), &data)
+	data := Alprd{}
+	err = json.Unmarshal([]byte(jsonData), &data)
 	content := string(jsonData)
 
 	// Print the contents of the file
@@ -27,5 +30,7 @@ func main() {
 		log.Println(err)
 		return
 	}
-    fmt.Println((content))
+    fmt.Println(content)
+	fmt.Println("Hobbies: ", data.Uuid)
+
 }
