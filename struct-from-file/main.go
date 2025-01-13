777a99d6e9411955d7e37dea10ecdc9a86369a96
Henok Wehibe <henokwehibe@gmail.com>
2025-01-13 07:55:56 +0000

    learnig struct

--- struct-from-file/main.go
+++ struct-from-file/main.go
@@ -8,25 +8,28 @@ import (
 )
 
 type Person struct {
-   Name    string
-   Age     int64
-   Hobbies []string
+	Name    string
+	Age     int64
+	Hobbies []string
 }
 
 func main() {
-   filePath := "db.json" 
-   jsonData, err := os.ReadFile(filePath) 
-   jsonStr := "{\"name\": \"Prashant\", \"age\": 27, \"hobbies\":[\"sports\",\"music\"]}"
-   data := Person{}
-   err = json.Unmarshal([]byte(jsonStr), &data)
-       content := string(jsonData)
+	filePath := "db.json"
+	jsonData, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonStr := "{\"name\": \"Prashant\", \"age\": 27, \"hobbies\":[\"sports\",\"music\"]}"
+	data := Person{}
+	err = json.Unmarshal([]byte(jsonStr), &data)
+	content := string(jsonData)
 
-        // Print the contents of the file
-        fmt.Println(content)
-   if err != nil {
-      log.Println(err)
-      return
-   }
+	// Print the contents of the file
+	fmt.Println(content)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-   fmt.Println(data.Name)
+	fmt.Println(data.Name)
 }
