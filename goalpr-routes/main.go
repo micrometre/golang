97d191f31a43d23f29e425c3d781da7c31f2916d
Henok Wehibe <henokwehibe@gmail.com>
2025-01-16 11:47:12 +0000

    sorting folders

--- goalpr-routes/main.go
+++ goalpr-routes/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"github.com/gin-gonic/examples/group-routes/routes"
+	"example/web-service-gin/routes"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+// Import the route packages
+
 func main() {
+
+
 	router := gin.Default()
-	router.Static("/", "./public")
-	routes.Run()
+	router.Static("/public", "./public")
+	// Create a sample table (if not exists)
+
+
+	routes.UserRoutes(router)
+	routes.VideoRoutes(router)
+	routes.AlprdRoutes(router)
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world")
+	})
+	router.Run(":5000")
 }
