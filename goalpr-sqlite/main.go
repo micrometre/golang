db55114b33663ca4d3143879538c1cce557cba4a
Henok Wehibe <henokwehibe@gmail.com>
2025-01-17 10:43:11 +0000

    adding cmd

--- goalpr-sqlite/main.go
+++ goalpr-sqlite/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3" // Import the sqlite3 driver
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3" // Import the sqlite3 driver
 )
 
 type Alprd struct {
@@ -44,35 +48,61 @@ func main() {
 
 	router.POST("/alprd", func(c *gin.Context) {
 		var alprd Alprd
-
 		if err := c.BindJSON(&alprd); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body format"})
 			return
 		}
-
 		stmt, err := db.Prepare("INSERT INTO alprd (plate, uuid) VALUES (?, ?)")
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		defer stmt.Close()
-
 		var plate interface{}
 		if alprd.Results == nil || len(alprd.Results) <= 0 {
 			plate = nil // Set plate to nil if no results are present
 		} else {
 			plate = alprd.Results[0].Plate // Extract the plate from the first result
 		}
-        var imageUrl string = "http://127.0.0.1:5000/public/images/" + alprd.Uuid + ".jpg"
+		var imageUrl string = "http://127.0.0.1:5000/public/images/" + alprd.Uuid + ".jpg"
 
-		_, err = stmt.Exec(plate,imageUrl  )
+		_, err = stmt.Exec(plate, imageUrl)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
-
 		fmt.Println(imageUrl)
 		c.JSON(http.StatusCreated, gin.H{"message": "Data inserted successfully"})
+	})
+
+	router.POST("/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+		filename := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, filename); err != nil {
+			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+			return
+		}
+		cmd := exec.Command("ls", "-l") // Replace with the actual command
+
+		// Capture output
+		var stdout, stderr bytes.Buffer
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
+		err = cmd.Run()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "stderr": stderr.String()})
+			return
+		}
+
+		output := stdout.String()
+		fmt.Println(output)
+		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email)
 	})
 
 	router.GET("/video", func(c *gin.Context) {
