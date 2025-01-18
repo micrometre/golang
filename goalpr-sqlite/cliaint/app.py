e124906b75e8b31a000f1da9898a8ca7a99d909a
Henok Wehibe <henokwehibe@gmail.com>
2025-01-18 09:22:46 +0000

    sorting client

--- goalpr-sqlite/cliaint/app.py
+++ goalpr-sqlite/cliaint/app.py
@@ -1,12 +1,25 @@
 import sqlite3
 import pandas as pd
-connection = sqlite3.connect("../data/sqlite.db")
+import re
 
+# Connect to the SQLite database
+connection = sqlite3.connect('../data/sqlite.db')
 print(connection.total_changes)
+
 cursor = connection.cursor()
 rows = cursor.execute("SELECT plate, uuid FROM alprd").fetchall()
-df = pd.DataFrame({'col':rows})
-df.to_csv('../data/upload.csv', sep='\t', encoding='utf-8' )
-(df.to_string()) 
-print (df)
-print (df)
+
+# Create a pandas DataFrame
+df = pd.DataFrame(rows, columns=['plate', 'uuid'])
+
+# Define the regular expression for GB plates
+test_gb = '^[A-Z]{2}[0-9]{2}[A-Z]{3}$'  # Corrected pattern
+
+# Filter DataFrame for GB plates using boolean indexing
+gb_plates = df[df['plate'].str.match(test_gb)]
+
+# Create a dictionary with plate as key and uuid as value from the filtered DataFrame
+plates = gb_plates.set_index('plate')['uuid'].to_dict()
+
+print(plates)
+#df.to_csv('../data/upload.csv', sep='\t', encoding='utf-8' )
